c5b9cbb13c387ed10fc16df231801f7940deea82
Ben Moody <nathanielmoody@live.com>
2024-07-14 10:42:23 -0400

    seal kontrollerversions class

--- src/Kontroller.API/Models/KontrollerVersion.cs
+++ src/Kontroller.API/Models/KontrollerVersion.cs
@@ -1,6 +1,6 @@
 namespace Kontroller.API.Models;
 
-internal class KontrollerVersion(string name, string version)
+internal sealed class KontrollerVersion(string name, string version)
 {
     public string Name { get; set; } = name;
     public string VersionNumber { get; set; } = version;
