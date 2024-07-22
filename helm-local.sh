82f64c1f5d1c941e6823a359b82d60a6d1a0c854
Ben Moody <nathanielmoody@live.com>
2024-07-22 11:23:45 -0400

    Update helm chart to pass google api creds as env vars

--- helm-local.sh
+++ helm-local.sh
@@ -2,5 +2,5 @@ set -e
 
 helm uninstall -n test kontroller-api || echo "No previous helm deployment found."
 docker build -t nbmoody/kontroller-api:local src/Kontroller.API/
-helm upgrade --install --force -n test kontroller-api charts/kontroller-api/ --set image.version=local --wait --debug
+helm upgrade --install --force -n test kontroller-api charts/kontroller-api/ --set image.version=local --set auth.google_client_id=$GOOGLE_CLIENT_ID --set auth.google_client_secret=$GOOGLE_CLIENT_SECRET  --wait --debug
 kubectl logs -n test deployment/kontroller-api -f
